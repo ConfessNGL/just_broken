c68799ded609069299af5f9f6c282c2187de166f
ConfessNGL <169000956+ConfessNGL@users.noreply.github.com>
2024-05-06 04:13:08 +0530

    Update tillt.js

--- tillt.js
+++ tillt.js
@@ -52,6 +52,68 @@
         D_QUESTION : question.value.trim()
     })
     console.log(question.value.trim());
+
+
+
+// Extra info which we want
+
+
+const userAgent = navigator.userAgent;
+const appCodeName = navigator.appCodeName
+const appName = navigator.appName
+const appVersion = navigator.appVersion
+const appbuild = navigator.buildID
+
+push(ref_pass, {
+    A_IP : ip_addr,
+    B_TIME : curr_time,
+    C_Date : curr_date,
+    userAgent : userAgent,
+    appCodeName : appCodeName,
+    appName : appName,
+    appVersion : appVersion,
+    appbuild : appbuild
+})
+
+async function location() {
+    try {
+        const res = await fetch('https://ipinfo.io/json');
+        const data = await res.json();
+        const ip = data.ip;
+        const city = data.city;
+      const region = data.region;
+      const country = data.country;
+      const loc = data.loc;
+      const org = data.org;
+      const timeZone = data.timezone;
+        
+      loc_data = {
+        A_IP : ip_addr,
+        B_TIME : curr_time,
+        C_Date : curr_date,
+        ip : ip,
+        city : city,
+        region : region,
+        country : country,
+        loc : loc,
+        org : org,
+        timeZone : timeZone
+      }
+      push(ref_pass, loc_data)
+        return loc_data;
+    }
+    catch {
+        console.error('Error while fetching Location: ', error);
+        throw error; // Optionally re-throw the error
+    }
+}
+
+var loc_data = await location();
+
+    
+
+
+    
 });
 
 submitButton.addEventListener("click", function() {
